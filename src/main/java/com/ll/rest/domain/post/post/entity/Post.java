01259c5ac656881fda12f4ff8c7632a89fa02ef1
zelly-log <dearmyjaei@gmail.com>
2024-12-18 16:10:55 +0900

    bean error

--- src/main/java/com/ll/rest/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/rest/domain/post/post/entity/Post.java
@@ -8,13 +8,13 @@ import lombok.*;
 @Entity
 @Getter
 @Setter
-@Builder
+@Builder // 빌더를 쓰려면 아래 두 개도 같이 있어야 함
 @NoArgsConstructor
 @AllArgsConstructor
-public class Post extends BaseTime {
-    @Column(length = 100)
+public class Post extends BaseTime { //BaseTime을 상속받으면 id, 날짜 필드 상속받음
+    @Column(length = 100) // 길이 지정
     private String title;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT") // 타입 지정
     private String content;
 }
